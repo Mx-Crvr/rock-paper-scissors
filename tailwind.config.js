ec0909a384b33b10752fa91171063e05c8e986f9
Mx-Crvr <mxcrvr31@gmail.com>
2023-05-11 13:13:18 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
